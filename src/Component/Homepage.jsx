e99f24ef850c47665d44098bc7131fc52e519424
ravirt7911 <ravirt7911@gmail.com>
2024-04-13 21:59:16 +0530

    firstpart done

--- src/Component/Homepage.jsx
+++ src/Component/Homepage.jsx
@@ -1,7 +1,49 @@
-import React from "react";
-import "./Homepage.css";
+// src/components/Homepage.js
+
+import React, { useState, useEffect } from "react";
+import { HotKeys } from "react-hotkeys";
+
 const Homepage = () => {
-  return <div className="home-page">Homepage</div>;
+  const [sequence, setSequence] = useState("");
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      handleSequenceChange(e.key);
+    };
+
+    document.addEventListener("keypress", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }, [sequence]);
+
+  const handleSequenceChange = (key) => {
+    setSequence((prevSequence) => prevSequence + key);
+    console.log(sequence);
+    if ((sequence + key).toLowerCase() === "cssbattle") {
+      alert("You won!");
+      console.log("You won!");
+    }
+  };
+
+  const keyMap = {
+    LETTER: "a b c d e f g h i j k l m n o p q r s t u v w x y z",
+  };
+
+  const handlers = {
+    LETTER: (key) => {
+      handleSequenceChange(key);
+    },
+  };
+
+  return (
+    <div className="home-page" tabIndex="0">
+      <HotKeys keyMap={keyMap} handlers={handlers}>
+        <div>Type "cssbattle"</div>
+      </HotKeys>
+    </div>
+  );
 };
 
 export default Homepage;
